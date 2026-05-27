73b947d45348cefa2c41557a9241996d0f60cf60
2025050136 <rlorv1213@kau.kr>
2026-05-27 15:11:13 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
+
     int N;
     int a[20] = {};
     int b[20] = {};
@@ -10,30 +11,21 @@ int main(void) {
 
     scanf("%d", &N);
 
-
+    
     for (p = a; p < a + N; p++) {
         scanf("%d", p);
     }
 
-
+   
     for (q = b; q < b + N; q++) {
         scanf("%d", q);
     }
-
-    
+   
     p = a;
-
-    
     q = b + N - 1;
 
-   
     while (p < a + N) {
-
-       
-        if (p == a + N - 1)
-            printf("%d", *p + *q);
-        else
-            printf("%d ", *p + *q);
+        printf(" %d", *p + *q);
 
         p++;
         q--;
